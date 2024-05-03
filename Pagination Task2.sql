d9d2ada1956019a125dd66260000ad9a59324dca
rohinikirpal <167945174+rohinikirpal@users.noreply.github.com>
2024-05-03 19:47:24 +0530

    Add files via upload

---
+++ Pagination Task2.sql
@@ -0,0 +1,10 @@
+select * from customer
+
+
+select * from customer limit 10 offset 0
+select * from customer limit 10 offset 10
+select * from customer limit 10 offset 20
+select * from customer limit 10 offset 30
+
+
+
